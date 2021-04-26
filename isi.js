7505e914dc35917324a7d811626244a7859efd0e
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2021-04-26 23:59:12 +0800

    Create isi.js

---
+++ isi.js
@@ -0,0 +1,5 @@
+document.writeln("<center>");
+document.writeln("<h1>Pemesanan Makanan</h1>");
+document.writeln("<h2>RM Nurul</h2>");
+document.writeln("<hr width=550 color=blue>");
+document.writeln("</center>");
